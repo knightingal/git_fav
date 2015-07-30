71d4aff6b9c3e4ee054f4c6b674087fa089074b8
knightingal <yujianming1985@126.com>
2015-07-30 22:47:19 +0800

    index page

--- local1000site/views.py
+++ local1000site/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import RequestContext, loader
+from .models import PicRepertory
 # Create your views here.
 def index(requeset):
-    return HttpResponse("this is local1000/index")
+    # return HttpResponse("this is local1000/index")
+    pic_repertories = PicRepertory.objects.all();
+    template = loader.get_template('local1000site/index.html')
+    context = RequestContext(requeset, {
+        'pic_repertories': pic_repertories,
+    })
+    return HttpResponse(template.render(context))
