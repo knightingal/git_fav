a67bb59a6fb89b148cf421792b19bf31c2b7125a
knightingal <yujianming1985@126.com>
2015-08-03 23:47:27 +0800

    recv post req

--- local1000site/views.py
+++ local1000site/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from .models import PicRepertory
+import json
+import codecs
 # Create your views here.
 def index(requeset):
     # return HttpResponse("this is local1000/index")
@@ -13,5 +15,13 @@ def index(requeset):
     return HttpResponse(template.render(context))
 
 def urls1000(request):
-    print request.get_full_path()
+    request_body = request.body.decode('utf-8')
+    request_obj = json.loads(request_body, 'uft-8')
+    title = request_obj["title"]
+    request_body_fmt = json.dumps(request_obj, ensure_ascii=False, indent=2)
+    print request_body_fmt
+    # print title
+    fp = codecs.open(title, 'w',  'utf-8')
+    fp.write(title)
+    fp.close()
     return HttpResponse("get urls1000")
