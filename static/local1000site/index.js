3d5868dec0f7b87a628d67f1d9f32ad73208371c
Knightingal <yujianming1985@hotmail.com>
2017-02-18 23:48:25 +0800

    set rep_list class to div node

--- static/local1000site/index.js
+++ static/local1000site/index.js
@@ -17,10 +17,12 @@
     function mOver(event) {
         document.getElementById("img_container").style.display = "block";
         document.getElementById("repertory").style.display = "none";
-        var index = event.target.attributes.aindex.value;
-        var picname = event.target.attributes.apicname.value;
-        var imgNode = document.getElementById("quick_img");
-        imgNode.src="/static/" + index + "/" + picname;
+        if (event.target.className==='rep_list') {
+            var index = event.target.children[0].attributes.aindex.value;
+            var picname = event.target.children[0].attributes.apicname.value;
+            var imgNode = document.getElementById("quick_img");
+            imgNode.src="/static/" + index + "/" + picname;
+        }
     }
 
     function mDown(obj) {
@@ -30,7 +32,7 @@
         document.getElementById("img_container").style.display = "none";
         document.getElementById("repertory").style.display = "block";
         document.getElementById("repertory").innerHTML = "";
-        var local1000Req = new Request("/local1000/picContentAjax/?id=" + obj.attributes.rep_id.value)
+        var local1000Req = new Request("/local1000/picContentAjax/?id=" + obj.children[0].attributes.rep_id.value)
         fetch(local1000Req)
             .then(function(resp){
                 return resp.json();
